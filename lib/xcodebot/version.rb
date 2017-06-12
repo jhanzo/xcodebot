7b5668473f135e324697f5e2ccc655c7dc52ae06
Dev CI <dev.ci@voyageprive.com>
2017-06-12 14:31:36 +0200

    update version

--- lib/xcodebot/version.rb
+++ lib/xcodebot/version.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 
 module Xcodebot
   NAME = "xcodebot"
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
   SUMMARY = "Run xcode bots"
   DESCRIPTION = "Xcodebot helps you to manage xcode bots easier"
   GITHUB = "https://github.com/j2shnz/xcodebot"
